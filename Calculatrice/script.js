0f52eb91d860329e42442fa6efbcb6aa3051a379
glennNTM <glennntoutoume8@gmail.com>
2024-12-23 11:32:54 +0100

    Add files via upload

---
+++ Calculatrice/script.js
@@ -0,0 +1,28 @@
+
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    
+    display.value += input;
+
+
+}
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate(){
+    try{
+
+        display.value = eval(display.value)
+    }
+
+
+    catch(error){
+        display.value = "Erreur"
+
+    }
+}
